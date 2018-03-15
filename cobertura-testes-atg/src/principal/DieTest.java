928e3bf0908d7f33d3e0a8f739520599d160fff2
MateusAugustoSS <mateus.augusto.santos@ccc.ufcg.edu.br>
2018-03-15 16:29:24 -0300

    Complexity 100%

---
+++ cobertura-testes-atg/src/principal/DieTest.java
@@ -0,0 +1,99 @@
+package principal;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class DieTest {
+	@Test
+	public void testConstrutorSemParametro() {
+		new Die();
+		new Die(2);
+		new Die(2, 1);
+	}
+	
+	@Test
+	public void testRoll() {
+		Die die = new Die();
+		die.roll();
+	}
+	
+	@Test
+	public void testGetNumSides() {
+		Die die = new Die();
+		die.getNumSides();
+	}
+
+	@Test
+	public void testGetResult() {
+		Die die = new Die();
+		die.getResult();
+	}
+	
+	
+	@Test
+	public void testToString() {
+		Die die = new Die();
+		die.toString();
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void TestConstrutorResultadoMaiorLados() {
+		Die die = new Die(2, 5);
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void TestConstrutorLadosInvalidos() {
+		Die die = new Die(1, 0);
+	}
+	
+	@Test
+	public void testEquals2() {
+		Die die = new Die(6, 3);
+		Die dae = new Die(6, 2);
+		die.equals(dae);
+	}
+	@Test
+	public void testEquals() {
+		Die die = new Die();
+		die.equals(new Die());
+		die.equals(null);
+		die.equals(die);
+		die.equals(new Object());
+	}
+	@Test
+	public void testEqualsNull() {
+		Die die = new Die();
+		die.equals(null);			
+	}
+	
+	@Test
+	public void testEqualsClassesDiferentes() {
+		Die die = new Die();
+		String str = new String();
+			
+		die.equals(str);
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void testConstrutorInvalido() {
+		Die die = new Die(-1, 4); 
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void testConstrutorConstrutorSidesInvalido() {
+		Die die = new Die(-1);
+	}
+	
+	@Test
+	public void testEqualsIguais() {
+		Die die = new Die();
+		die.equals(die);
+	}
+	
+	@Test
+	public void testEquals3() {
+		Die die = new Die(5, 3);
+		Die dae = new Die(6, 3);
+		die.equals(dae);
+	}
+}
